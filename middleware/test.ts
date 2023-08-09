db37b1ffe19bf311d48ac100d6b33ecd90317a34
Ivo Kovacevic <ikovacevic96k@gmail.com>
2023-08-09 16:19:17 +0200

    Abort navigation

--- middleware/test.ts
+++ middleware/test.ts
@@ -1,6 +1,7 @@
 export default defineNuxtRouteMiddleware((to, from) => {
   console.log("Inside test middleware");
-  const random = Math.random() < 0.2;
-  console.log("Random: ", random);
-  if (!random) return;
+  const random = Math.random() < 0.5;
+  console.log("Abort: ", !random);
+  if (!random) return abortNavigation();
+  console.log("Navigation happened");
 });

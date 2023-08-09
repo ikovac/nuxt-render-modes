08c52ac6596012bde602869007f89593b7ea188c
Ivo Kovacevic <ikovacevic96k@gmail.com>
2023-08-09 16:11:31 +0200

    Add random access mw logic

--- middleware/test.ts
+++ middleware/test.ts
@@ -1,3 +1,6 @@
 export default defineNuxtRouteMiddleware((to, from) => {
   console.log("Inside test middleware");
+  const random = Math.random() < 0.2;
+  console.log("Random: ", random);
+  if (!random) return;
 });
